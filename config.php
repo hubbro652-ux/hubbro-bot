5dffc6912e258b44d0cc815895745566b87996a3
hubbro652-ux <hubbro652@gmail.com>
2026-06-24 22:09:57 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,1 @@
-<?php
 
-$host = "sql305.byetcluster.com";
-$user = "if0_42248008";
-$pass = "Shashika1092736";
-$db   = "if0_42248008_Hubpro";
-
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    die("Database Connection Failed: " . $conn->connect_error);
-}
-
-?>
